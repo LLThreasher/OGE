a184baf3750b780c19910600841cb7213bbe9ccc
Jiuru Li <lixue9jiu@gmail.com>
2026-07-23 17:33:45 +0800

    add versioning for macos and android

--- cmake/build_config.cmake
+++ cmake/build_config.cmake
@@ -9,6 +9,46 @@ execute_process(
     OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
+# Default values
+set(MARKETING_VERSION "0.0.0")
+set(BUILD_NUMBER "0")
+set(COMMIT_HASH "")
+set(DIRTY_FLAG "")
+
+# Split by "-"
+string(REPLACE "-" ";" GIT_PARTS "${GIT_HASH}")
+list(LENGTH GIT_PARTS PART_COUNT)
+
+if(PART_COUNT EQUAL 1)
+    # Exactly on a tag: v1.4.2
+    string(REGEX REPLACE "^v" "" MARKETING_VERSION "${GIT_HASH}")
+
+elseif(PART_COUNT GREATER_EQUAL 3)
+    # v1.4.2-15-gabc1234[-dirty]
+
+    list(GET GIT_PARTS 0 TAG_PART)
+    list(GET GIT_PARTS 1 BUILD_NUMBER)
+    list(GET GIT_PARTS 2 HASH_PART)
+
+    string(REGEX REPLACE "^v" "" MARKETING_VERSION "${TAG_PART}")
+    string(REGEX REPLACE "^g" "" COMMIT_HASH "${HASH_PART}")
+
+    if(PART_COUNT GREATER 3)
+        list(GET GIT_PARTS 3 DIRTY_FLAG)
+    endif()
+endif()
+
+execute_process(
+    COMMAND ${GIT_EXECUTABLE} rev-list --count HEAD
+    OUTPUT_VARIABLE BUILD_NUMBER
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+message(STATUS "Marketing Version: ${MARKETING_VERSION}")
+message(STATUS "Build Number: ${BUILD_NUMBER}")
+message(STATUS "Commit Hash: ${COMMIT_HASH}")
+message(STATUS "Dirty: ${DIRTY_FLAG}")
+
 # ----------------------------
 # Detect preset
 # ----------------------------
@@ -32,9 +72,15 @@ endif()
 # ----------------------------
 # Combined build tag
 # ----------------------------
+if(NOT DIRTY_FLAG)
 set(BUILD_TAG
-    "${GIT_HASH}_${BUILD_PRESET}"
+    "${MARKETING_VERSION}.${BUILD_NUMBER}_${COMMIT_HASH}_${BUILD_PRESET}"
 )
+else()
+set(BUILD_TAG
+    "${MARKETING_VERSION}.${BUILD_NUMBER}_${COMMIT_HASH}_${DIRTY_FLAG}_${BUILD_PRESET}"
+)
+endif()
 
 # ----------------------------
 # Output directory for generated headers
