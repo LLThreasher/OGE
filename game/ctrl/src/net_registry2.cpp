e5a5891f0b322cf4dda411e4e075923b37bd1c50
Jiuru Li <lixue9jiu@gmail.com>
2026-08-06 14:26:24 +0800

    registry refactor

---
+++ game/ctrl/src/net_registry2.cpp
@@ -0,0 +1,475 @@
+// #include <cassert>
+// #include <cstddef>
+// #include <cstdint>
+// #include <vector>
+
+// #include "game/components.hpp"
+// #include "game/input/player_input_stream.hpp"
+// #include "game/replication_registry2.hpp"
+// #include "game/terrain/defs.hpp"
+// #include "game/terrain/terrain_view.hpp"
+// #include "game/input/entity_event_stream.hpp"
+// #include "oge/fmt.hpp"
+// #include "oge/log.hpp"
+// #include "oge/point3.hpp"
+// #include "oge/runtime/net_packet_sender.hpp"
+// #include "oge/runtime/typed_registry.hpp"
+
+// using oge::runtime::AnythingFactory;
+// using oge::runtime::SendType;
+// using game::EntityNetStream;
+
+// static void RegisterEntityReplication(AnythingFactory& af)
+// {
+//     auto& desc = af.RegisterType<game::ReplicatedTag>();
+//     desc.capabilities.template Add<game::ReplicationCapability>(
+//         af.Id<game::ReplicatedTag>(), &game::EntityReplication::Encode,
+//         &game::EntityReplication::Decode, &game::EntityReplication::CreateState,
+//         SendType::Reliable, 0);
+// }
+
+// static void RegisterTerrainReplication(AnythingFactory& af)
+// {
+//     auto& desc = af.RegisterType<game::terrain::TerrainView>();
+//     desc.capabilities.template Add<game::ReplicationCapability>(
+//         af.Id<game::terrain::TerrainView>(), &game::TerrainReplication::Encode,
+//         &game::TerrainReplication::Decode,
+//         &game::TerrainReplication::CreateState, SendType::Reliable, 1);
+// }
+
+// template <typename T>
+// static void RegisterEventReplication(AnythingFactory& af)
+// {
+//     auto& desc = af.RegisterType<T>();
+//     desc.capabilities.template Add<game::ReplicationCapability>(
+//         af.Id<T>(), &game::EntityEventStreamReplication<T>::Encode,
+//         &game::EntityEventStreamReplication<T>::Decode,
+//         &game::EntityEventStreamReplication<T>::CreateState, SendType::Reliable,
+//         0);
+// }
+
+// template <typename T>
+// static void RegisterComponentReplication(AnythingFactory& af)
+// {
+//     auto& desc = af.RegisterType<T>();
+//     desc.capabilities.template Add<game::ReplicationCapability>(
+//         af.Id<T>(), &game::ComponentReplication<T>::Encode,
+//         &game::ComponentReplication<T>::Decode,
+//         &game::ComponentReplication<T>::CreateState, SendType::Reliable, 0);
+// }
+
+// void game::RegisterReplications(AnythingFactory& af, ReplicationRegistry& rf)
+// {
+//     RegisterTerrainReplication(af);
+//     RegisterEntityReplication(af);
+//     RegisterComponentReplication<ComponentAABBCollider>(af);
+//     RegisterComponentReplication<ComponentCamera>(af);
+//     RegisterComponentReplication<ComponentPerspectiveCamera>(af);
+//     RegisterComponentReplication<ComponentPhysicBody>(af);
+//     RegisterComponentReplication<ComponentCreature>(af);
+//     RegisterComponentReplication<ComponentPlayer>(af);
+//     RegisterEventReplication<input::PlayerInputStream>(af);
+//     rf.RegisterFrom(af);
+// }
+
+// void game::InstallTerrainReplicationHooks(entt::registry& world)
+// {
+// }
+
+// void game::InstallEntityReplicationHooks(entt::registry& world)
+// {
+//     world.ctx().emplace<EntityNetStream>();
+
+//     world.on_construct<ReplicatedTag>()
+//         .template connect<
+//             +[](entt::registry& world, entt::entity e)
+//             {
+//                 LOG_DEBUG("create entity {}", static_cast<uint64_t>(e));
+
+//                 world.ctx()
+//                     .template get<EntityNetStream>()
+//                     .Push({input::EntityEventType::Create, e});
+//             }>();
+
+//     world.on_destroy<ReplicatedTag>()
+//         .template connect<
+//             +[](entt::registry& world, entt::entity e)
+//             {
+//                 LOG_DEBUG("destroy entity {}", static_cast<uint64_t>(e));
+
+//                 world.ctx()
+//                     .template get<EntityNetStream>()
+//                     .Push({input::EntityEventType::Destroy, e});
+//             }>();
+// }
+
+// void game::ApplyEntityReplicationEvents(entt::registry& world)
+// {
+//     auto* stream = world.ctx().find<EntityNetStream>();
+//     if (!stream)
+//     {
+//         return;
+//     }
+
+//     static EntityNetStream::Cursor cursor = 0;
+
+//     input::EntityEvent event{};
+
+//     while (stream->PollOne(cursor, event))
+//     {
+//         switch (event.type)
+//         {
+//             case input::EntityEventType::Create:
+//             {
+//                 if (!world.valid(event.entity))
+//                 {
+//                     auto created = world.create(event.entity);
+//                     assert(created == event.entity);
+//                 }
+
+//                 break;
+//             }
+
+//             case input::EntityEventType::Destroy:
+//             {
+//                 if (world.valid(event.entity))
+//                 {
+//                     world.destroy(event.entity);
+//                 }
+
+//                 break;
+//             }
+//         }
+//     }
+// }
+
+// using namespace game;
+
+// void EntityReplication::Encode(entt::registry& world, ENetPeer* peer,
+//                                oge::runtime::NetPacketSender& server,
+//                                FamilyId family, SendType sendType,
+//                                uint8_t channel, entt::any& anyState)
+// {
+//     auto& state = entt::any_cast<State&>(anyState);
+
+//     auto* stream = world.ctx().find<input::EntityEventStream>();
+//     if (!stream) return;
+
+//     if (state.useSnapshot)
+//     {
+//         for (auto e : world.view<ReplicatedTag>())
+//         {
+//             size_t size = sizeof(FamilyId) + sizeof(input::EntityEventType) +
+//                           sizeof(entt::entity);
+
+//             LOG_DEBUG("send entity {}", (uint32_t)e);
+//             auto packet = server.StartPacket(size);
+
+//             packet.Write(family);
+//             packet.Write(EntityEventType::Create);
+//             packet.Write(e);
+
+//             server.Send(peer, packet);
+//         }
+//         state.useSnapshot = false;
+//         stream->AdvanceCursor(state.cursor);
+//     }
+//     else
+//     {
+//         input::EntityEvent delta;
+
+//         while (stream->PollOne(state.cursor, delta))
+//         {
+//             size_t size = sizeof(FamilyId) + sizeof(input::EntityEventType) +
+//                           sizeof(entt::entity);
+
+//             auto packet = server.StartPacket(size);
+
+//             packet.Write(family);
+//             packet.Write(delta.type);
+//             packet.Write(delta.entity);
+
+//             server.Send(peer, packet, sendType, channel);
+//         }
+//     }
+// }
+
+// void EntityReplication::Decode(entt::registry& world, net::Buffer& buffer)
+// {
+//     input::EntityEventType type;
+//     buffer.Read(type);
+
+//     entt::entity entity;
+//     buffer.Read(entity);
+
+//     switch (type)
+//     {
+//         case input::EntityEventType::Create:
+//         {
+//             LOG_DEBUG("recive entity {}", (uint32_t)entity);
+//             auto e = world.create(entity);
+//             LOG_DEBUG("created entity {}", (uint32_t)e);
+//             assert(e == entity);
+//             break;
+//         }
+
+//         case input::EntityEventType::Destroy:
+//         {
+//             if (world.valid(entity)) world.destroy(entity);
+//             break;
+//         }
+//     }
+// }
+
+// constexpr size_t MAX_CHUNK_PER_FRAME = 4;
+
+// enum class ChunkPacketType : uint8_t
+// {
+//     Load,
+//     LoadCompressed,
+//     Update,
+//     Discard,
+// };
+
+// entt::any TerrainReplication::CreateState()
+// {
+//     return State{};
+// }
+
+// static std::pmr::vector<uint8_t> CompressChunk(const std::uint8_t* data,
+//                                                std::size_t size)
+// {
+//     std::pmr::vector<uint8_t> out{};
+
+//     if (size == 0) return out;
+
+//     out.reserve(size);
+
+//     std::size_t i = 0;
+
+//     while (i < size)
+//     {
+//         std::uint8_t value = data[i];
+//         std::uint8_t count = 1;
+
+//         while (i + count < size && data[i + count] == value && count < 255)
+//         {
+//             ++count;
+//         }
+
+//         out.push_back(count);
+//         out.push_back(value);
+
+//         i += count;
+//     }
+
+//     return out;
+// }
+
+// static void DecompressChunk(const std::uint8_t* compressed,
+//                             std::size_t compressedSize, std::uint8_t* out,
+//                             std::size_t expectedSize)
+// {
+//     std::size_t inOffset = 0;
+//     std::size_t outOffset = 0;
+
+//     while (inOffset < compressedSize)
+//     {
+//         if (inOffset + 2 > compressedSize)
+//         {
+//             assert(false && "Invalid RLE data");
+//         }
+
+//         std::uint8_t count = compressed[inOffset++];
+//         std::uint8_t value = compressed[inOffset++];
+
+//         if (outOffset + count > expectedSize)
+//         {
+//             assert(false && "RLE decompressed data too large");
+//         }
+
+//         std::memset(out + outOffset, value, count);
+//         outOffset += count;
+//     }
+
+//     if (outOffset != expectedSize)
+//     {
+//         assert(false && "RLE decompressed size mismatch");
+//     }
+// }
+
+// void TerrainReplication::Encode(entt::registry& world, ENetPeer* peer,
+//                                 oge::runtime::NetPacketSender& server,
+//                                 FamilyId family, SendType sendType,
+//                                 uint8_t channel, entt::any& anyState)
+// {
+//     using namespace terrain;
+//     auto& state = entt::any_cast<State&>(anyState);
+//     auto& terrain = world.ctx().get<terrain::TerrainView>();
+
+//     auto writeAddChunk = [&](const ChunkData* chunk)
+//     {
+//         PaletteCompressedChunk cChunk;
+//         PaletteCompressedChunk::FromChunkData(*chunk, cChunk);
+//         // LOG_DEBUG("send hash {}, {}", chunk->Coords, DebugHash(cChunk));
+
+//         auto compressedData =
+//             CompressChunk(reinterpret_cast<const std::uint8_t*>(cChunk.data),
+//                           CHUNK_SIZE_TOTAL);
+//         auto ptype = compressedData.size() < CHUNK_SIZE_TOTAL
+//                          ? ChunkPacketType::LoadCompressed
+//                          : ChunkPacketType::Load;
+
+//         auto packet =
+//             server.StartPacket(CHUNK_SIZE_TOTAL + 512 * sizeof(uint32_t));
+//         packet.Write(family);
+//         packet.Write(ptype);
+//         packet.Write(chunk->Coords);
+//         packet.Write((uint32_t)cChunk.palette.size());
+//         packet.WriteRaw(cChunk.palette.data(),
+//                         cChunk.palette.size() * sizeof(uint32_t));
+//         if (ptype == ChunkPacketType::LoadCompressed)
+//         {
+//             packet.Write((uint32_t)compressedData.size());
+//             packet.WriteRaw(compressedData.data(), compressedData.size());
+//         }
+//         else
+//         {
+//             packet.WriteRaw(cChunk.data, CHUNK_SIZE_TOTAL);
+//         }
+//         server.Send(peer, packet, sendType, 0);
+//     };
+
+//     auto writeUpdateChunk =
+//         [&](const ChunkStateUpdateEvent& e, const ChunkData* chunk)
+//     {
+//         // LOG_DEBUG("send update {}", chunk->Coords);
+//         auto packet =
+//             server.StartPacket(CHUNK_SIZE_TOTAL + 512 * sizeof(uint32_t));
+//         packet.Write(family);
+//         packet.Write(ChunkPacketType::Update);
+//         packet.Write(chunk->Coords);
+//         packet.Write(e.packed.dirtyCnt);
+//         for (uint8_t i = 0; i < e.packed.dirtyCnt; ++i)
+//         {
+//             packet.Write(e.dirtyBlocks.at(i));
+//             oge::LocalPoint3 pt = e.dirtyBlocks.at(i);
+//             packet.Write(chunk->GetBlock(pt.x, pt.y, pt.z));
+//         }
+//         server.Send(peer, packet, sendType, 0);
+//     };
+
+//     if (state.needsSnapshot)
+//     {
+//         if (!state.snapshotCursor.IsValid())
+//         {
+//             terrain.GetEvents().AdvanceCursor(state.chunkEventCursor);
+//         }
+//         size_t counter = 0;
+//         for (ChunkHandle& cursor = state.snapshotCursor;
+//              auto chunk = terrain.PollChunk(cursor);)
+//         {
+//             if (chunk->state != ChunkState::Persistent) continue;
+
+//             writeAddChunk(chunk);
+
+//             if (++counter >= MAX_CHUNK_PER_FRAME) return;
+//         }
+//         state.needsSnapshot = false;
+//         return;
+//     }
+
+//     size_t counter = 0;
+//     ChunkStateUpdateEvent e;
+//     while (terrain.GetEvents().PollOne(state.chunkEventCursor, e))
+//     {
+//         auto chunk = terrain.GetChunk(e.chunk);
+//         if (!chunk || chunk->state != e.packed.state) continue;
+//         if (e.packed.state == ChunkState::Persistent)
+//         {
+//             if (e.IsAllDirty())
+//             {
+//                 writeAddChunk(chunk);
+//             }
+//             else if (e.packed.dirtyCnt > 0)
+//             {
+//                 writeUpdateChunk(e, chunk);
+//             }
+
+//             if (++counter >= MAX_CHUNK_PER_FRAME) return;
+//         }
+//     }
+// }
+
+// void TerrainReplication::Decode(entt::registry& world, net::Buffer& buffer)
+// {
+//     using namespace terrain;
+//     auto& terrain = world.ctx().get<terrain::TerrainView>();
+
+//     auto readAddChunk = [&](ChunkPacketType ptype, oge::Point3 coord)
+//     {
+//         uint32_t paletteSize;
+//         buffer.Read(paletteSize);
+//         PaletteCompressedChunk chunk;
+//         chunk.palette.resize(paletteSize);
+//         buffer.ReadRaw(chunk.palette.data(), paletteSize * sizeof(uint32_t));
+
+//         // LOG_DEBUG("recv hash {}, {}", coord, DebugHash(chunk));
+//         auto handle = terrain.CreateChunk(coord);
+//         if (ptype == ChunkPacketType::Load)
+//         {
+//             buffer.ReadRaw(chunk.data, CHUNK_SIZE_TOTAL);
+//         }
+//         else
+//         {
+//             uint32_t size;
+//             buffer.Read(size);
+//             std::pmr::vector<uint8_t> data(size);
+//             buffer.ReadRaw(data.data(), size);
+//             DecompressChunk(data.data(), size, chunk.data, CHUNK_SIZE_TOTAL);
+//         }
+//         chunk.ToChunkData(*terrain.GetChunk(handle));
+//         return handle;
+//     };
+
+//     auto readUpdateChunk = [&](oge::Point3 coord)
+//     {
+//         auto [handle, chunk] = terrain.GetChunk(coord);
+//         uint8_t cnt;
+//         buffer.Read(cnt);
+//         for (uint8_t i = 0; i < cnt; ++i)
+//         {
+//             oge::CompactLocalPoint3 pos;
+//             buffer.Read(pos);
+//             oge::LocalPoint3 lp = pos;
+//             uint32_t blkVal;
+//             buffer.Read(blkVal);
+//             terrain.SetBlock(lp.x + (coord.x << 4), lp.y + (coord.y << 4),
+//                              lp.z + (coord.z << 4), blkVal);
+//         }
+//         return handle;
+//     };
+
+//     ChunkPacketType ptype;
+//     buffer.Read(ptype);
+//     oge::Point3 coord;
+//     buffer.Read(coord);
+
+//     ChunkHandle handle;
+//     switch (ptype)
+//     {
+//         case ChunkPacketType::Load:
+//         case ChunkPacketType::LoadCompressed:
+//             handle = readAddChunk(ptype, coord);
+//             break;
+//         case ChunkPacketType::Update:
+//             handle = readUpdateChunk(coord);
+//             break;
+//         default:
+//             assert(false);
+//             break;
+//     }
+//     if (ptype == ChunkPacketType::Update)
+//         terrain.DowngradeChunk(handle, ChunkState::InvalidLighting);
+//     terrain.UpgradeChunk(handle, ChunkState::Persistent);
+// }
